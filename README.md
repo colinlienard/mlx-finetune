bfa9aabae6e03df0fe3f980947a25767a0976d26
Colin Lienard <colin.lienard87@gmail.com>
2025-04-27 22:02:11 +0200

    feat: use codestral

--- README.md
+++ README.md
@@ -1,65 +1,83 @@
 # mlx-fine-tune
 
+## Data Preparation
+
 Download dataset:
 
 ```bash
-huggingface-cli download HuggingFaceH4/ultrachat_200k --repo-type dataset --local-dir dataset
+huggingface-cli download dataset-name --repo-type dataset --local-dir dataset
 ```
 
 Format data:
 
 ```bash
-bun format.ts
+bun format.ts --input dataset.jsonl
 ```
 
-Then split data:
+## Fine-tuning
 
-```bash
-bun split.ts
-```
-
-Train:
+Install `mlx`:
 
 ```bash
 pip install mlx-lm
 ```
 
+Train:
+
 ```bash
 mlx_lm.lora --train \
---model ~/.cache/huggingface/hub/models--mistralai--Mistral-7B-Instruct-v0.2/snapshots/3ad372fc79158a2148299e3318516c786aeded6c \
+--model ./codestral-quantized \
 --data data \
---batch-size 1
+--batch-size 1 \
+--num-layers 4 \
+--test
 ```
 
-Test:
+Convert to quantized if not enough memory while training:
+
+```bash
+mlx_lm.convert --hf-path mistralai/Codestral-22B-v0.1 -q --mlx-path codestral-quantized
+```
+
+Prompt:
 
 ```bash
 mlx_lm.generate \
---model ~/.cache/huggingface/hub/models--mistralai--Mistral-7B-Instruct-v0.2/snapshots/3ad372fc79158a2148299e3318516c786aeded6c \
+--model ./codestral-quantized \
 --max-tokens 500 \
 --adapter-path adapters \
---prompt "Create a basic svelte counter component"
+--prompt "Create a basic svelte counter component using runes"
 ```
+
+## Use in Ollama
 
 Merge to existing model:
 
 ```bash
 mlx_lm.fuse \
---model ~/.cache/huggingface/hub/models--mistralai--Mistral-7B-Instruct-v0.2/snapshots/3ad372fc79158a2148299e3318516c786aeded6c \
+--model ./codestral-quantized \
 --adapter-path adapters \
 --save-path fused \
 --de-quantize
 ```
 
-Convert to gguf:
+Convert to `.gguf`:
+
+<details>
+
+<summary>Setup llama.cpp</summary>
 
 ```bash
-cd ..
 git clone git@github.com:ggml-org/llama.cpp.git
 cd llama.cpp
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+```
+
+</details>
+
+```bash
 python convert_hf_to_gguf.py ../mlx-fine-tune/fused \
 --outfile ../mlx-fine-tune/mymodel.gguf \
 --outtype q8_0
--- format.ts
+++ format.ts
@@ -1,27 +1,60 @@
-import { writeFileSync } from "fs";
+import csv from "csvtojson";
 
-interface Conversation {
-  from: string;
-  value: string;
-}
+const args = process.argv.slice(2).flatMap((arg) => arg.split("="));
+const input = arg("input");
+if (!input) throw new Error("Input file path is required");
 
-interface ConversationData {
-  conversations: Conversation[];
-}
+const raw = await Bun.file(input).text();
 
-const raw = await Bun.file("./dataset/raw.jsonl").text();
-const lines = raw.split("\n").filter((line) => line.trim() !== "");
+const rawLines = await (async () => {
+  switch (input.split(".").pop()) {
+    case "jsonl":
+      return parseJSONL(raw);
+    case "csv":
+      return await parseCSV(raw);
+    default:
+      throw new Error("Unsupported file format");
+  }
+})();
 
-const formattedLines: string[] = lines.map((line) => {
-  const data: ConversationData = JSON.parse(line);
-  const prompt =
-    data.conversations.find((conv) => conv.from === "human")?.value || "";
-  const completion =
-    data.conversations.find((conv) => conv.from === "gpt")?.value || "";
+const lines: string[] = rawLines.map(([prompt, completion]) => {
   const text = "[INST]" + prompt + "[/INST]" + completion;
   return JSON.stringify({ text });
 });
 
-const formattedText = formattedLines.join("\n");
+const train = lines.slice(0, (lines.length / 10) * 6);
+const valid = lines.slice((lines.length / 10) * 6, (lines.length / 10) * 8);
+const test = lines.slice((lines.length / 10) * 8);
 
-writeFileSync("./dataset/formatted.jsonl", formattedText);
+await Promise.all([
+  Bun.write("./data/train.jsonl", train.join("\n")),
+  Bun.write("./data/valid.jsonl", valid.join("\n")),
+  Bun.write("./data/test.jsonl", test.join("\n")),
+]);
+
+function arg(option: string) {
+  const pathArgIndex = args.indexOf("--" + option);
+  if (pathArgIndex === -1) return;
+  if (pathArgIndex + 1 < args.length) {
+    return args[pathArgIndex + 1];
+  }
+  return args[pathArgIndex];
+}
+
+function parseJSONL(raw: string): [string, string][] {
+  const lines = raw.split("\n").filter((line) => line.trim() !== "");
+  return lines.map((line) => {
+    const data: { conversations: { from: string; value: string }[] } =
+      JSON.parse(line);
+    const prompt =
+      data.conversations.find((conv) => conv.from === "human")?.value || "";
+    const completion =
+      data.conversations.find((conv) => conv.from === "gpt")?.value || "";
+    return [prompt, completion];
+  });
+}
+
+async function parseCSV(raw: string): Promise<[string, string][]> {
+  const lines: Record<string, string>[] = await csv().fromString(raw);
+  return lines.map((line) => Object.values(line)) as [string, string][];
+}
